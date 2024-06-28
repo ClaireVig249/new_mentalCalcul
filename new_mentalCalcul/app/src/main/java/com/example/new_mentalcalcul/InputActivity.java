337fd85a36bb49e50073625dda48b714affbd34a
ClaireViguoine <claire.viguoine@etud.u-picardie.fr>
2024-06-28 18:14:00 +0200

    ajout fichier toolbar.xml

--- new_mentalCalcul/app/src/main/java/com/example/new_mentalcalcul/InputActivity.java
+++ new_mentalCalcul/app/src/main/java/com/example/new_mentalcalcul/InputActivity.java
@@ -12,6 +12,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.new_mentalcalcul.entities.Score;
+
 public class InputActivity extends AppCompatActivity {
 
     private String nomJoueur;
